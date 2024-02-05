51db1585018157b27a921f3f9975e46728eeec8a
Kingcoder44 <skushagra645@gmail.com>
2024-02-05 19:37:36 +0530

    Added comments for better understanding

--- Dfs.java
+++ Dfs.java
@@ -1,55 +1,72 @@
 import java.util.Iterator;
 import java.util.LinkedList;
-class Dfs{
-    // class Graph{
-        int v;
-        LinkedList<Integer> adj[];
-        Dfs(int v)
-        {
-            this.v=v;
-            adj=new LinkedList[v];
 
-        for(int i=0;i<v;i++)
-            adj[i] =new LinkedList<>();
-        }
-    void addEdge(int v,int u)
-    {
+class Dfs {
+    // Input: Number of vertices
+    int v;
+    
+    // Adjacency list to represent the graph
+    LinkedList<Integer> adj[];
+    
+    // Constructor to initialize the graph with a given number of vertices
+    Dfs(int v) {
+        this.v = v;
+        adj = new LinkedList[v];
+
+        // Initialize each adjacency list
+        for (int i = 0; i < v; i++)
+            adj[i] = new LinkedList<>();
+    }
+
+    // Method to add a directed edge from vertex 'v' to vertex 'u'
+    void addEdge(int v, int u) {
         adj[v].add(u);
     }
-    public void dfsutil(int v,boolean visited[]){
-        visited[v]=true;
-        System.out.print(v+" ");
+
+    // Recursive utility function for DFS traversal starting from a given vertex
+    public void dfsutil(int v, boolean visited[]) {
+        // Mark the current vertex as visited and print it
+        visited[v] = true;
+        System.out.print(v + " ");
     
+        // Recur for all the vertices adjacent to this vertex
         Iterator<Integer> itr = adj[v].listIterator();
-        while(itr.hasNext())
-        {
+        while(itr.hasNext()) {
             int n = itr.next();
-            if(!visited[n])
-            dfsutil(n, visited);
+            if (!visited[n])
+                dfsutil(n, visited);
         }
     }
     
-    void dfsTra(int v)
-    {
+    // Method to perform DFS traversal starting from a given vertex
+    void dfsTra(int v) {
         boolean[] visited = new boolean[this.v];
-        dfsutil(v,visited);
+        dfsutil(v, visited);
     }
-    public static void main(String args[])
-    {
+
+    // Main method to test the DFS implementation
+    public static void main(String args[]) {
+        // Create a graph with 4 vertices
         Dfs g = new Dfs(4);
-        g.addEdge(0,1);
-        g.addEdge(0,2);
-        g.addEdge(1,2);
-        g.addEdge(2,0);
-        g.addEdge(2,3);
-        g.addEdge(3,3);
-        System.out.println("Dfs from vertex 0: ");
+
+        // Add directed edges to the graph
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        // Perform DFS traversal starting from different vertices
+        System.out.println("DFS from vertex 0: ");
         g.dfsTra(0);
         System.err.println();
-        System.out.println("Dfs from vertex 1: ");
+
+        System.out.println("DFS from vertex 1: ");
         g.dfsTra(1);
         System.err.println();
-        System.out.println("Dfs from vertex 2: ");
+
+        System.out.println("DFS from vertex 2: ");
         g.dfsTra(2);
         System.out.println();
     }
