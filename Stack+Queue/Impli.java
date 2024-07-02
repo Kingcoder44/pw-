9cc5f5404092e9920a7ac8c57d29d1ba04c8f31c
Kingcoder44 <113973573+Kingcoder44@users.noreply.github.com>
2024-07-03 00:09:35 +0530

    Add files via upload

---
+++ Stack+Queue/Impli.java
@@ -0,0 +1,23 @@
+package Stack;
+import java.util.*;
+//stack implementation using stack inbuilt function
+public class Impli {
+    public static void main(String[] args) {
+        Scanner ob = new Scanner(System.in);
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        System.out.println(s);
+        s.pop();
+        System.out.println(s.pop()+s.pop());
+        s.peek();
+        System.out.println(s.search(3));
+   //    s.remove(2);
+        System.out.println(s);
+
+
+    }
+}
